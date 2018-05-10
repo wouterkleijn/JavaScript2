d49d13eaf1dc031a31f80fd44c68bc58b9ac70d0
Wouter <wouterkleijn@gmail.com>
2018-05-10 23:30:03 +0200

    first edits

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,30 @@
-'use strict';
-{
-  const bookTitles = [
-    // Replace with your own book titles
-    'harry_potter_chamber_secrets'
-  ];
+const bookNames = [
+  //'harry_potter_chamber_of_secrets',
+  'congo',
+  //'the_bridge',
+  //'amsterdam',
+  'the_future_of_freedom',
+  //'between_the_world_and_me',
+];
 
 
-  // Replace with your own code
-  console.log(bookTitles);
+const bookData = {
+  "the_future_of_freedom": {
+    title: "The Future of Freedom",
+    language: "English",
+    author: "Fareed Zakaria",
+  },
+  "congo": {
+    title: "Congo",
+    language: "English",
+    author: "David van Reybrouck",
+  }
+};
+
+for (const i in bookNames) {
+  const book = bookData[i];
+  const ul = document.getElementById("bookTitles");
+  const li = document.createElement('li');
+  li.innerHTML = book.title;
+  ul.appendChild(li);
 }
